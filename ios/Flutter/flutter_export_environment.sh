9829140ae09b041054a374158d1d6dc4521c7cbe
Alex Fourman <fourman.alex@gmail.com>
2019-09-27 13:40:20 +0100

    updated iOS files after building on a mac

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/user158700/Downloads/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/user158700/StudioProjects/Drillz"
+export "FLUTTER_TARGET=/Users/user158700/StudioProjects/Drillz/lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/user158700/Downloads/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_BUILD_NAME=0.0.1"
+export "FLUTTER_BUILD_NUMBER=4"
+export "TRACK_WIDGET_CREATION=true"
