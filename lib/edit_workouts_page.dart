106fbe8a3b5335485d41f02ce61cf7a34aaa308f
Alex Fourman <fourman.alex@gmail.com>
2021-04-11 16:03:31 +0300

    wip no longer resize scaffold

--- lib/edit_workouts_page.dart
+++ lib/edit_workouts_page.dart
@@ -14,6 +14,7 @@ class EditWorkoutsPage extends StatelessWidget {
   Widget build(BuildContext context) {
     final repo = context.watch<Repository>();
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       appBar: AppBar(
         title: const Text('Edit Workouts'),
       ),
